1cec1533fd23d29d8f13b3f5e15f470103769be4
wwoelmer <wwoelmer@vt.edu>
2025-06-27 12:30:37 +1200

    fix capitalization in axis, add Rose's edits

--- scripts/MS/11_deltaAIC_compare_temporal_aggregations.R
+++ scripts/MS/11_deltaAIC_compare_temporal_aggregations.R
@@ -10,9 +10,9 @@ out$id_covar <- factor(out$id_covar,
                        levels = c("bottom_DRP_ugL", "bottom_NH4_ugL", "temp_C_8",
                                   "air_temp_mean", "windspeed_min", "monthly_avg_level_m",
                                   "sum_alum", "none"),
-                       labels = c("Bottom DRP", "Bottom NH4", "Bottom Water Temp",
-                                  "Mean Air Temp", "Min Windspeed", "Water Level", 
-                                  "Alum Dosed", "None"))
+                       labels = c("Bottom DRP", "Bottom NH4", "Bottom water temp",
+                                  "Mean air temp", "Min windspeed", "Water level", 
+                                  "Alum dosed", "None"))
 
 # calculate the difference across variables
 out_prop <- out %>% 
@@ -40,12 +40,12 @@ delta_aic <- ggplot(out_prop, aes(x = as.Date(start_date), y = diff_from_none_ai
   facet_wrap(~id_covar) +
   theme_bw() +
   ylab(expression(Delta~AIC[c])) +
-  xlab('Start of Iteration') +
+  xlab('Window start date') +
   theme(text=element_text(size=14)) +
   #scale_color_manual(values = col_pal) +
   scale_color_manual(values = c('#6C5379', '#5FAD56')) 
 delta_aic
-ggplotly(delta_aic)
+
 ################################################################################
 # repeat for three discrete window
 out_three <- read.csv('./data/model_output_three_windows.csv')
@@ -54,9 +54,9 @@ out_three$id_covar <- factor(out_three$id_covar,
                              levels = c("bottom_DRP_ugL", "bottom_NH4_ugL", "temp_C_8",
                                         "air_temp_mean", "windspeed_min", "monthly_avg_level_m",
                                         "sum_alum", "none"),
-                             labels = c("Bottom DRP", "Bottom NH4", "Bottom Water Temp",
-                                        "Mean Air Temp", "Min Windspeed", "Water Level", 
-                                        "Alum Dosed", "None"))
+                             labels = c("Bottom DRP", "Bottom NH4", "Bottom water temp",
+                                        "Mean air temp", "Min windspeed", "Water level", 
+                                        "Alum dosed", "None"))
 
 # calculate the difference across variables
 out_prop3 <- out_three %>% 
@@ -101,9 +101,9 @@ out_full$id_covar <- factor(out_full$id_covar,
                             levels = c("bottom_DRP_ugL", "bottom_NH4_ugL", "temp_C_8",
                                        "air_temp_mean", "windspeed_min", "monthly_avg_level_m",
                                        "sum_alum", "none"),
-                            labels = c("Bottom DRP", "Bottom NH4", "Bottom Water Temp",
-                                       "Mean Air Temp", "Min Windspeed", "Water Level", 
-                                       "Alum Dosed", "None"))
+                            labels = c("Bottom DRP", "Bottom NH4", "Bottom water temp",
+                                       "Mean air temp", "Min windspeed", "Water level", 
+                                       "Alum dosed", "None"))
 
 # calculate the difference across variables
 out_prop_full <- out_full %>% 
@@ -145,7 +145,7 @@ p_si <- ggarrange(delta_aic_full, delta_aic3, common.legend = TRUE,
           labels = 'auto')
 p_si
 
-ggsave('./figures/resubmission/si_figs/full_discrete_signif_aic.png', p_si,
+ggsave('./figures/MS/si_figs/full_discrete_signif_aic.png', p_si,
        dpi = 300, units = 'mm', height = 300, width = 400, scale = 0.6)
 
 ################################################################################
@@ -153,51 +153,76 @@ ggsave('./figures/resubmission/si_figs/full_discrete_signif_aic.png', p_si,
 out_prop_all <- full_join(out_prop, out_prop3)
 out_prop_all <- full_join(out_prop_all, out_prop_full)
 
+out_prop_all <- out_prop_all %>%
+  mutate(Window = case_when(timeperiod == "Discrete window" ~ "Discrete", 
+                            timeperiod == "Full window" ~ "Full", 
+                            timeperiod == "Moving window" ~ "Moving", 
+                            .default = timeperiod))
 
-p1 <- ggplot() +
-  geom_rect(data = out_prop_all[out_prop_all$timeperiod=='Moving window' 
+(p1 <- ggplot() +
+  geom_rect(data = out_prop_all[out_prop_all$Window=='Moving' 
                                 & out_prop_all$id_covar!='None',],
             aes(xmin = min(as.Date(start_date)), xmax = max(as.Date(end_date)), 
-                ymin = -2, ymax = 2), alpha = 0.8, fill = "grey") +
-  geom_segment(data = out_prop_all[out_prop_all$timeperiod=='Moving window'
+                ymin = -2, ymax = 2), alpha = 0.8, fill = "grey85") +
+  geom_segment(data = out_prop_all[out_prop_all$Window=='Moving'
                                    & out_prop_all$id_covar!='None',],
                aes(x = as.Date(start_date), xend = as.Date(end_date),
                    y = diff_from_none_aic, yend = diff_from_none_aic,
-                   color = timeperiod)) +
-  geom_segment(data = out_prop_all[out_prop_all$timeperiod!='Moving window'
+                   color = Window)) +
+  geom_segment(data = out_prop_all[out_prop_all$Window!='Moving'
                                    & out_prop_all$id_covar!='None',],
-            aes(x = as.Date(start_date), xend = as.Date(end_date),
-                y = diff_from_none_aic, yend = diff_from_none_aic,
-                color = timeperiod), linewidth = 2, alpha = 0.9) +
+               aes(x = as.Date(start_date), xend = as.Date(end_date),
+                   y = diff_from_none_aic, yend = diff_from_none_aic,
+                   color = Window), linewidth = 2, alpha = 0.9) +
   geom_hline(yintercept = 0) +
-  facet_wrap(~id_covar, scales = 'free') +
+  facet_wrap(~id_covar, scales = 'free_y', ncol = 3) +
   theme_bw() +
-  ylab(expression(Delta~AIC[c])) +
-  xlab('Date') +
-  theme(text=element_text(size=14)) +
-  #scale_color_manual(values = col_pal) +
-  scale_color_manual(values = c('#fdae61','#d73027', '#4575b4')) 
+    labs(y=(expression(Delta~AIC[c])), x='\nWindow start date') +
+  theme(strip.background = element_blank(), text = element_text(size = 14),
+        strip.text = element_text(size = 14, hjust = 0), 
+        panel.grid.minor = element_blank())+
+    theme(legend.position = c(1, 0),
+          legend.justification = c(1, 0))+
+  scale_color_manual(values = c('#fdae61','#d73027', '#4575b4')) )
 
-p1
 
-ggsave('./figures/resubmission/all_windows_delta_aic.png', p1,
-       dpi = 300, units = 'mm', height = 300, width = 400, scale = 0.6)
 
-p2 <- ggplot(out_prop_all, aes(x = as.Date(start_date), y = diff_from_none_aic)) +
+ggsave('./figures/MS/si_figs/all_windows_delta_aic.png', p1,
+       dpi = 300, units = 'mm', height = 400, width = 400, scale = 0.6)
+
+
+
+(p2 <- ggplot(out_prop_all, aes(x = as.Date(start_date), y = diff_from_none_aic)) +
   geom_rect(data = out_prop_all[out_prop_all$timeperiod=='Moving window',],
             aes(xmin = min(as.Date(start_date)), xmax = max(as.Date(start_date)), 
-                ymin = -2, ymax = 2), alpha = 0.8, fill = "grey") +
+                ymin = -2, ymax = 2), alpha = 0.8, fill = "grey85") +
   geom_point(aes(color = significant)) +
   geom_hline(yintercept = 0) +
-  facet_wrap(~id_covar, scales = 'free') +
+  facet_wrap(~id_covar, scales = 'free_y', ncol = 1) +
   theme_bw() +
   ylab(expression(Delta~AIC[c])) +
-  xlab('Start of Iteration') +
-  theme(text=element_text(size=14)) +
-  #scale_color_manual(values = col_pal) +
-  scale_color_manual(values = c('#6C5379', '#5FAD56')) 
+  xlab('\nWindow start date') +
+  theme(strip.background = element_blank(), text = element_text(size = 14),
+        strip.text = element_text(size = 14, hjust = 0), 
+        panel.grid.minor = element_blank())+
+    theme(legend.justification = "top" , 
+          plot.margin = margin(0, 0, 0, 0, "points"),  axis.ticks.x = element_blank(),
+          axis.title = element_text(size = 12, colour = "black"), 
+          axis.text = element_text(size = 9), 
+          panel.grid = element_blank())+
+  scale_color_manual(values = c('#CC79A7' ,'#009E73'), name = "Significant"))
 
-p2
 
-ggsave('./figures/resubmission/movingwindow_delta_aic_significance.png', p2,
-       dpi = 300, units = 'mm', height = 300, width = 400, scale = 0.6)
+ggsave('./figures/MS/movingwindow_delta_aic_significance.png', p2,
+       dpi = 300, units = 'mm', height = 500, width = 350, scale = 0.6)
+
+
+
+
+
+
+
+
+
+
+
